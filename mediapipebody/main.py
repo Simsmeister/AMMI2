cae6c2c2b16bd8ddb1add32cd6fa49e3e2cef00a
Zextir <gabriel.deangelis007@gmail.com>
2023-11-09 13:05:13 +0100

    python stuff

---
+++ mediapipebody/main.py
@@ -0,0 +1,17 @@
+#pipe server
+from body import BodyThread
+import time
+import mediapipe as mp
+import struct
+import global_vars
+from sys import exit
+
+
+thread = BodyThread()
+thread.start()
+
+i = input()
+print("Exiting…")        
+global_vars.KILL_THREADS = True
+time.sleep(0.5)
+exit()
