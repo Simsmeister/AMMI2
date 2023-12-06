fb8468ac15c80c97a26d0e703265ae4b05229db5
Simsmeister <rr67wh@student.aau.dk>
2023-12-06 21:34:34 +0100

    Trainer Audio Implementeret

---
+++ Assets/Scripts/TrainerSound.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainerSound : MonoBehaviour
+{
+    public AudioClip velkomstKlip;
+
+    public AudioClip[] almostClips;
+
+    public AudioClip[] completedClips;
+
+    public AudioSource trainerAudioSource;
+
+    public GameObject pipeServerObject;
+
+    public PipeServer pipeServer;
+
+    public bool playedSeven = false;
+    public bool playedTen = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        trainerAudioSource = this.gameObject.GetComponent<AudioSource>();
+        pipeServer = pipeServerObject.GetComponent<PipeServer>();
+        trainerAudioSource.PlayOneShot(velkomstKlip);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pipeServer.Counter == 7 && !playedSeven)
+            {
+                trainerAudioSource.PlayOneShot(almostClips[Random.Range(0, 3)]);
+                playedSeven = true;
+            }
+        else if (pipeServer.Counter == 10 && !playedTen)
+            {
+                trainerAudioSource.PlayOneShot(completedClips[Random.Range(0, 3)]);
+                playedTen = true;
+            }
+    }
+}
